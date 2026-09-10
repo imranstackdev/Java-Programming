8ddd805f33adeb4e81375dc20f5cdeab5db58e79
Imran Ali <134768731+imranstackdev@users.noreply.github.com>
2026-09-10 17:50:30 +0000

    Day 2 Q5

---
+++ reverseNumber.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class reverseNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number to print reverse : ");
+        int num = sc.nextInt();
+        while (num > 0) {
+            int digit = num % 10;
+            System.out.println(digit);
+            num = num / 10;
+        }
+
+        sc.close();
+    }
+}

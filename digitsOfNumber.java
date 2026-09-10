d698167c5f9233b45e10a83f76bc538328421a06
Imran Ali <134768731+imranstackdev@users.noreply.github.com>
2026-09-10 17:38:21 +0000

    Day2 Q4

---
+++ digitsOfNumber.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class digitsOfNumber{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number to print in verticle : ");
+        int n = sc.nextInt();
+
+        int rev = 0;
+
+        while(n!=0){
+            int digit = n % 10;
+            rev = rev * 10 + digit;
+            n = n/10;
+        }
+
+        while(rev > 0){
+            int digit = rev % 10;
+            System.out.println(digit);
+            rev = rev/10;
+        }
+        sc.close();
+    }
+}
